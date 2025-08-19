f5f9f9fd15ca1d180bef9482d09a342c42a6e63c
Beatriz <beatriz.mota@aluno.ifsp.edu.br>
2025-08-19 18:30:12 -0300

    Finalização_Atividade

--- Atividades/Atividade_17/calculadora.gd
+++ Atividades/Atividade_17/calculadora.gd
@@ -2,10 +2,18 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	print("Clicado")
 	
 	if($OptionButton.get_selected_id() ==0):
-		$resultado.text = str(int($numero1.text)+ int($numero2.text))
+		$resultado.text = "O resultado é: " + str(int($numero1.text)+ int($numero2.text))
+		
+	elif($OptionButton.get_selected_id() ==1):
+		$resultado.text = "O resultado é: " + str(int($numero1.text)-int($numero2.text))
+		
+	elif($OptionButton.get_selected_id() ==2):
+		$resultado.text = "O resultado é: " + str(int($numero1.text)* int($numero2.text))
+		
+	elif($OptionButton.get_selected_id() ==3):
+		$resultado.text = "O resultado é: " + str(int($numero1.text)/ int($numero2.text))
+		
 	else:
-		$resultado.text = $numero1.text-$numero2.text
-	pass # Replace with function body.
+		$resultado.text = "Selecione apenas números inteiros."
