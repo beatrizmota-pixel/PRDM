18e1b3c1dc478512a599f6fb404de47284bc2be8
Beatriz <beatriz.mota@aluno.ifsp.edu.br>
2025-09-09 12:09:07 -0300

    09/09- Atividade_18

--- Atividades/Atividade_17/calculadora.gd
+++ Atividades/Atividade_17/calculadora.gd
@@ -1,20 +1,14 @@
-
 extends Control
 
-
+#Lógica da calculadora pela manipulação dos huds:
 func _on_button_pressed() -> void:
-	
 	if($OptionButton.get_selected_id() ==0):
 		$resultado.text = "O resultado é: " + str(int($numero1.text)+ int($numero2.text))
-		
 	elif($OptionButton.get_selected_id() ==1):
 		$resultado.text = "O resultado é: " + str(int($numero1.text)-int($numero2.text))
-		
 	elif($OptionButton.get_selected_id() ==2):
 		$resultado.text = "O resultado é: " + str(int($numero1.text)* int($numero2.text))
-		
 	elif($OptionButton.get_selected_id() ==3):
 		$resultado.text = "O resultado é: " + str(int($numero1.text)/ int($numero2.text))
-		
 	else:
 		$resultado.text = "Digite apenas números inteiros e não esqueça de selecionar a operação."
--- Atividades/Atividade_18/atividade_18.gd
+++ Atividades/Atividade_18/atividade_18.gd
@@ -1,14 +1,24 @@
 extends Control
 
+#Formas de Manipular o preenchimento:
 
-
+#1º: Com um timer
 func _on_timer_timeout() -> void:
-	print("Oi")
-	$TextureProgressBar.value += 1
-	pass # Replace with function body.
+	$TextureProgressBar.value += 10
+	pass 
 
-
+#2º: Com um botão
 func _on_button_pressed() -> void:
-	print("tedte")
-	$TextureProgressBar.value += 1
-	pass # Replace with function body.
+	$TextureProgressBar.value += 10
+	pass 
+
+#3º: Com o próprio Texture Progress Bar
+func _on_texture_progress_bar_value_changed(value: float) -> void:
+	if $TextureProgressBar.value >= $TextureProgressBar.max_value:
+		$TextureProgressBar.max_value = $TextureProgressBar.max_value * 1.2
+		$TextureProgressBar.value = 0
+		$TextureProgressBar/Label.text = str(int($TextureProgressBar/Label.text) + 1)
+	pass
+	
+	
+	
