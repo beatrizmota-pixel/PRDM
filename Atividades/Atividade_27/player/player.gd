5662dc7e8ee952df2b0260748bc8a5b9a6b226ff
carinaqueiroz1 <carina.queiroz@aluno.ifsp.edu.br>
2025-11-11 12:28:08 -0300

    Update player.gd

--- Atividades/Atividade_27/player/player.gd
+++ Atividades/Atividade_27/player/player.gd
@@ -25,8 +25,11 @@ func die():
 	$AnimatedSprite2D.animation = "hurt"
 	set_process(false)
 
-
-
-
-func _on_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+func _on_area_entered(area):
+	if area.is_in_group("coins"):
+		area.pickup()
+		pickup.emit()
+	if area.is_in_group("obstacles"):
+		hurt.emit()
+		die()
+		
